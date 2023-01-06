e23cb40e3c007a3495d8d47921c2c8f5b6837659
sikim07 <skim88.1942@gmail.com>
2023-01-06 14:12:52 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-3xl font-bold underline text-green-600 ">
-      <h1 className="text-3xl font-bold underline">test</h1>test
+    <div className="text-3xl font-bold underline text-green-600 text-center relative">
+      <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]">test</div>test
     </div>
   )
-}
+} 
