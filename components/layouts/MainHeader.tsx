aaa8ed0185c777cd2f5247fb93f5e7a8c1e3da5a
sikim07 <skim88.1942@gmail.com>
2023-01-06 19:35:47 +0900

    add header navigation bar

--- components/layouts/MainHeader.tsx
+++ components/layouts/MainHeader.tsx
@@ -1,3 +1,3 @@
 export default function MainHeader() {
-    return <div>Header</div>
+    return <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">Header</nav>
 }
