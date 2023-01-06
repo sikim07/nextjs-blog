d8a25f655ee4dc56fad0a80cbbdf36a04db1ae75
sikim07 <skim88.1942@gmail.com>
2023-01-06 15:50:40 +0900

    tailwind test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="text-3xl font-bold underline text-green-600 text-center relative">
       <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]">test</div>test
+      <div>teaildsaf</div>
     </div>
   )
 } 
