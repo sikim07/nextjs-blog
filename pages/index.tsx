509b4482eb49d1a3a73e8f957fba7e98782a78d3
sikim07 <skim88.1942@gmail.com>
2023-01-16 08:27:40 +0900

    add code prettier-ed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import MainBaseLayout from '../components/layouts/MainBaseLayout'
 
 export default function Home() {
-  return (
-    <MainBaseLayout>
-    <div className="text-3xl font-bold underline text-green-600 text-center relative">
-      <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]">test</div>test
-      <div className='flex'>
-        <div className='flex-1'>
-          11
-        </div>
-        <div className='flex-1'>
-          22
-        </div>
-        </div>
-            <div>teaildsasdfdsadsfaf</div>            
-        </div>
-    </MainBaseLayout>
-  )
-} 
+    return (
+        <MainBaseLayout>
+            <div className="text-3xl font-bold underline text-green-600 text-center relative">
+                <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]">
+                    test
+                </div>
+                test
+                <div className="flex">
+                    <div className="flex-1">11</div>
+                    <div className="flex-1">22</div>
+                </div>
+                <div>teaildsasdfdsadsfaf</div>
+                <div>teaildsasdfdsadsfaf</div>
+                <div>teaildsasdfdsadsfaf</div>
+            </div>
+        </MainBaseLayout>
+    )
+}
