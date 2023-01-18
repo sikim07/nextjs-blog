4a3048f28653475114d60efb5fd9d97acae5d37c
sikim07 <skim88.1942@gmail.com>
2023-01-18 09:59:16 +0900

    2

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,6 @@ export default function Home() {
                 <div>teaildsasdfdsadsfaf</div>
                 <div>teaildsasdfdsadsfaf</div>
                 <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
             </div>
         </MainBaseLayout>
     )
