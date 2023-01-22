90e86bc68442b85158fc011a8a81f30f1650e44c
sikim07 <skim88.1942@gmail.com>
2023-01-22 19:16:35 +0900

    roll-back

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,12 +24,6 @@ export default function Home() {
                 test
                 <div className="flex">
                     <div className="flex-1">11</div>
-                    <div className="flex-1">11</div>
-                    <div className="flex-1">11</div>
-                    <div className="flex-1">11</div>
-                    <div className="flex-1">11</div>
-                    <div className="flex-1">11</div>
-                    <div className="flex-1">22</div>
                     <div className="flex-1">22</div>
                 </div>
                 <div>teaildsasdfdsadsfaf</div>
