9d91d4472785e7e9b105c2e4f8a15db54e8760ed
sikim07 <skim88.1942@gmail.com>
2023-01-26 18:44:00 +0900

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,42 @@
+import { SetStateAction, useEffect, useState } from 'react'
 import MainBaseLayout from '../components/layouts/MainBaseLayout'
 
-export default function Home() {
-    const fetchData = async () => {
-        const res1 = await fetch('/', {
-            method: 'GET',
-        })
-        console.log(res1)
-        const res2 = await fetch('/test', {
-            method: 'GET',
-        })
-        console.log(res2)
+interface apiResType1 {
+    response: string
+}
 
-        return res2
+export default function Home() {
+    const [res1, setRes1] = useState<SetStateAction<null | apiResType1>>(null)
+    const [res2, setRes2] = useState<SetStateAction<null | Response>>(null)
+
+    const fetchData = async () => {
+        const response1 = await fetch('/', {
+            method: 'GET',
+        })
+        setRes1(response1)
+        const response2 = await fetch('/test', {
+            method: 'GET',
+        })
+        setRes2(response2)
+        console.log(res2)
     }
 
-    fetchData()
-    return (
+    useEffect(() => {
+        fetchData()
+    }, [])
+    return res1 && res2 ? (
         <MainBaseLayout>
             <div className="text-3xl font-bold underline text-green-600 text-center relative">
-                <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]">
-                    test
-                </div>
+                <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]"></div>
                 test
                 <div className="flex">
+                    <div className="flex-1">11</div>
                     <div className="flex-1">11</div>
                 </div>
                 <div>teaildsasdfdsadsfaf</div>
             </div>
         </MainBaseLayout>
+    ) : (
+        <div>loading</div>
     )
 }
