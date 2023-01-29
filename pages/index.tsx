b59469e7fc2d254f0faceec0d0f4b2bede5a5181
jaden <skim88.1942@gmail.com>
2023-01-29 11:45:48 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,7 @@ export default function Home() {
                 <div>teaildsasdfdsadsfaf</div>
                 <div>teaildsasdfdsadsfaf</div>
                 <div>teaildsasdfdsadsfaf</div>
+                <div>teaildsasdfdsadsfaf</div>
             </div>
         </MainBaseLayout>
     ) : (
