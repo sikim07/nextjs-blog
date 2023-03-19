e4d297f0e95a6d40a0577d28bca8a7128fc967bd
sikim07 <skim88.1942@gmail.com>
2023-03-20 08:52:51 +0900

    rollback

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,19 +32,6 @@ export default function Home() {
                     <div className="flex-1">11</div>
                 </div>
                 <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
-                <div>teaildsasdfdsadsfaf</div>
             </div>
         </MainBaseLayout>
     ) : (
