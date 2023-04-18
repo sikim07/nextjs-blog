a5431144a90621f11f11e03b1d1a1102db4e8361
sikim07 <skim88.1942@gmail.com>
2023-04-18 17:23:48 +0900

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,21 +9,8 @@ export default function Home() {
     const [res1, setRes1] = useState<SetStateAction<null | apiResType1>>(null)
     const [res2, setRes2] = useState<SetStateAction<null | Response>>(null)
 
-    const fetchData = async () => {
-        const response1 = await fetch('/', {
-            method: 'GET',
-        })
-        const response2 = await fetch('/test', {
-            method: 'GET',
-        })
-        setRes2(response2)
-        console.log(res2)
-    }
-
-    useEffect(() => {
-        fetchData()
-    }, [])
-    return res1 && res2 ? (
+    useEffect(() => {}, [])
+    return (
         <MainBaseLayout>
             <div className="text-3xl font-bold underline text-green-600 text-center relative">
                 <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]"></div>
@@ -32,9 +19,8 @@ export default function Home() {
                     <div className="flex-1">11</div>
                 </div>
                 <div>teaildsasdfdsadsfaf</div>
+                <div>teaildsasdfdsadsfaf</div>
             </div>
         </MainBaseLayout>
-    ) : (
-        <div>loading</div>
     )
 }
