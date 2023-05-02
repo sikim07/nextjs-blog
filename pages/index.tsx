804fc3b8dcbca149323493378812c7b6b45047e8
sikim07 <skim88.1942@gmail.com>
2023-05-02 18:57:43 +0900

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,8 @@ export default function Home() {
                     <div className="flex-1">11</div>
                 </div>
                 <div>teaildsasdfdsadsfaf</div>
+                <div>teaildsasdfdsadsfaf</div>
+                <div>teaildsasdfdsadsfaf</div>
             </div>
         </MainBaseLayout>
     )
