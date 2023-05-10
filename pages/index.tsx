85655912b5a2901986fa6f334af1bec987261b5c
sikim07 <skim88.1942@gmail.com>
2023-05-10 22:06:37 +0900

    메인페이지 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,10 @@ interface apiResType1 {
 }
 
 export default function Home() {
-    const [res1, setRes1] = useState<SetStateAction<null | apiResType1>>(null)
-    const [res2, setRes2] = useState<SetStateAction<null | Response>>(null)
-
     useEffect(() => {}, [])
     return (
         <MainBaseLayout>
-            <div className="relative text-3xl font-bold text-center text-green-600 underline">
-                <div className="text-3xl font-bold underline hover:ring-2 w-50 h-2/6 w-[35px]"></div>
-                test
-                <div className="flex">
-                    <div className="flex-1">11</div>
-                </div>
-                <div>teaildsasdfdsadsfaf</div>
-            </div>
+            <div className="relative text-center text-3xl font-bold text-green-600 underline"></div>
         </MainBaseLayout>
     )
 }
