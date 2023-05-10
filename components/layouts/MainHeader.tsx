63859bd1ac1adecfffa14fdde2a42da0409742ff
sikim07 <skim88.1942@gmail.com>
2023-05-10 22:06:29 +0900

    header 수정

--- components/layouts/MainHeader.tsx
+++ components/layouts/MainHeader.tsx
@@ -1,3 +1,40 @@
-export default function MainHeader() {
-    return <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">Header</nav>
+import Image from 'next/image'
+import { NaviagtionItemType } from '@/types/ui'
+import icon_cat from '@/assets/icons/icon_cat.svg'
+
+const MainHeader = () => {
+    const navigationItems: NaviagtionItemType[] = [
+        {
+            id: 1,
+            title: 'nav1',
+            link: '/',
+        },
+        {
+            id: 2,
+            title: 'nav2',
+            link: '/',
+        },
+        {
+            id: 3,
+            title: 'nav3',
+            link: '/',
+        },
+    ]
+
+    return (
+        <nav className="h-[60px] w-full">
+            <div className="flex">
+                <Image src={icon_cat} width={50} height={50} alt="고양이 아이콘" />
+                {navigationItems.map((item: NaviagtionItemType) => (
+                    <div
+                        className="p-[5px_20px] text-[28px] font-bold text-[#333333]"
+                        key={item.id}>
+                        {item.title}
+                    </div>
+                ))}
+            </div>
+        </nav>
+    )
 }
+
+export default MainHeader
