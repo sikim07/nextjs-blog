2a5d6fbad6cc96182a3dc9aa511cb375e95739f1
sikim07 <skim88.1942@gmail.com>
2023-05-10 22:36:34 +0900

    불필요 navigation 제거

--- components/layouts/MainHeader.tsx
+++ components/layouts/MainHeader.tsx
@@ -1,37 +1,14 @@
 import Image from 'next/image'
-import { NaviagtionItemType } from '@/types/ui'
 import icon_cat from '@/assets/icons/icon_cat.svg'
+import Link from 'next/link'
 
 const MainHeader = () => {
-    const navigationItems: NaviagtionItemType[] = [
-        {
-            id: 1,
-            title: 'nav1',
-            link: '/',
-        },
-        {
-            id: 2,
-            title: 'nav2',
-            link: '/',
-        },
-        {
-            id: 3,
-            title: 'nav3',
-            link: '/',
-        },
-    ]
-
     return (
-        <nav className="h-[60px] w-full">
-            <div className="flex">
-                <Image src={icon_cat} width={50} height={50} alt="고양이 아이콘" />
-                {navigationItems.map((item: NaviagtionItemType) => (
-                    <div
-                        className="p-[5px_20px] text-[28px] font-bold text-[#333333]"
-                        key={item.id}>
-                        {item.title}
-                    </div>
-                ))}
+        <nav className="w-full">
+            <div className="p-[10px]">
+                <Link href={'/'}>
+                    <Image src={icon_cat} width={50} height={50} alt="고양이 아이콘" />
+                </Link>
             </div>
         </nav>
     )
