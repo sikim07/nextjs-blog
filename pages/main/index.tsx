6b3316c41f957f5dea9fe8a79edade168362a98f
sikim07 <skim88.1942@gmail.com>
2023-05-22 08:55:43 +0900

    rolbakc

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -2,18 +2,6 @@ const Main = () => {
     return (
         <div>
             <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
         </div>
     )
 }
