0b112356ef8c0341f68875ae086e5bddeabb32df
sikim07 <skim88.1942@gmail.com>
2023-05-30 08:48:07 +0900

    roolback

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -2,16 +2,6 @@ const Main = () => {
     return (
         <div>
             <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
-            <div>adsfkln</div>
         </div>
     )
 }
